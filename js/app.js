da05a91d2b688e001789b4bbf81d79b3efed71a4
yarg1985 <fanchao05@gmail.com>
2017-02-13 21:24:00 -0500

    editted

--- js/app.js
+++ js/app.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
 		welcomMsg: function() {
 			var path = window.location.pathname;
 			console.log(path);
-			if (path === "/") {
+			if (path === "/trivia_master/") {
 				setTimeout(function() {
 					App.showMessage($("#message1"));
 				}, 1500); 
